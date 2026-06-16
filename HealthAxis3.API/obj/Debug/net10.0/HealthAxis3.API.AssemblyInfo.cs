016681eba61730fcda8df93a89974de7ce8a9c8d
Melsy-M-J-UST <310054@ust.com>
2026-06-16 19:02:47 +0530

    Updated

--- HealthAxis3.API/obj/Debug/net10.0/HealthAxis3.API.AssemblyInfo.cs
+++ HealthAxis3.API/obj/Debug/net10.0/HealthAxis3.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthAxis3.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab9d6c230ece92957c32feb0b43572945c47c4a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33cf6afb4583f5f94b97f289b785b8b6e535b601")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthAxis3.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthAxis3.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
