1733e512fbd0c4702a7b99bbab42a7f63c156960
Melsy-M-J-UST <310054@ust.com>
2026-06-19 03:45:18 +0530

    Added get appointment by doctor and patient id in  repository layer

--- HealthAxis3.API/Repository/IAppointmentRepository.cs
+++ HealthAxis3.API/Repository/IAppointmentRepository.cs
@@ -4,5 +4,7 @@ namespace HealthAxis3.API.Repository
 {
     public interface IAppointmentRepository : IRepository<Appointment>
     {
+        Task<List<Appointment>> GetByDoctorIdAsync(int id, CancellationToken ct = default);
+        Task<List<Appointment>> GetByPatientIdAsync(int id, CancellationToken ct = default);
     }
 }
--- HealthAxis3.API/Repository/Implementation/AppointmentRepository.cs
+++ HealthAxis3.API/Repository/Implementation/AppointmentRepository.cs
@@ -1,13 +1,22 @@
 ﻿using HealthAxis3.API.Data;
 using HealthAxis3.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HealthAxis3.API.Repository.Implementation
 {
     public class AppointmentRepository(AppDbContext context) : Repository<Appointment>(context), IAppointmentRepository
     {
-
-        // get by id pat, doc
         //delete
         //update
+        public async Task<List<Appointment>> GetByDoctorIdAsync(int id, CancellationToken ct = default)
+        {
+            var existing = await context.Set<Appointment>().Where(e => e.DoctorId == id).ToListAsync(ct);
+            return existing;
+        }
+        public async Task<List<Appointment>> GetByPatientIdAsync(int id, CancellationToken ct = default)
+        {
+            var existing = await context.Set<Appointment>().Where(e => e.PatientId == id).ToListAsync(ct);
+            return existing;
+        }
     }
 }
