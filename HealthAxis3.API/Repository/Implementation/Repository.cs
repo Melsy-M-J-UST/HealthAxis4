2ea9798207ff2e6e347b215443e34794935cfd01
Melsy-M-J-UST <310054@ust.com>
2026-06-19 03:40:16 +0530

    Used primary Constructor

--- HealthAxis3.API/Repository/Implementation/Repository.cs
+++ HealthAxis3.API/Repository/Implementation/Repository.cs
@@ -2,32 +2,16 @@
 
 namespace HealthAxis3.API.Repository.Implementation
 {
-    public class Repository<T> : IRepository<T> where T : class
+    public class Repository<T>(DbContext context) : IRepository<T> where T : class
     {
-        private readonly DbContext _context;
+        private readonly DbContext _context = context;
 
-        public Repository(DbContext context)
-        {
-            _context = context;
-        }
         public async Task<T> CreateAsync(T entity, CancellationToken ct = default)
         {
             await _context.Set<T>().AddAsync(entity, ct);
             await _context.SaveChangesAsync(ct);
             return entity;
         }
-
-        //public async Task<T?> DeleteAsync(int id, CancellationToken ct = default)
-        //{
-        //    var existing = await _context.Set<T>().FindAsync([id], ct);
-        //    if (existing == null)
-        //    {
-        //        return null;
-        //    }
-        //    _context.Set<T>().Remove(existing);
-        //    await _context.SaveChangesAsync(ct);
-        //    return existing;
-        //}
 
         public async Task<List<T>> GetAllAsync(CancellationToken ct = default)
         {
