fda3d03e08a08021c087037142865a80ef672ce1
Melsy-M-J-UST <310054@ust.com>
2026-06-23 15:23:12 +0530

    Started Blazor UI and Updated Tests and Shared Class library

--- HealthAxis3.BlazorUI/Program.cs
+++ HealthAxis3.BlazorUI/Program.cs
@@ -1,4 +1,6 @@
 using HealthAxis3.BlazorUI;
+using HealthAxis3.BlazorUI.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7120") });
-
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(sp =>
+{
+    return sp.GetRequiredService<CustomAuthenticationStateProvider>();
+});
 await builder.Build().RunAsync();
